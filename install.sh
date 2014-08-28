8df2410a114cf19800093617cd18cfae798a3475
Juan Carlos Ferrer <juan.carlos@micronixsolutions.com>
2014-08-28 17:20:56 -0500

    Adds bashrc

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@
 #TODO: Learn sh foo, and make it a function....
 ln -s `pwd`/.tmux.conf ~/.tmux.conf
 ln -s `pwd`/.vimrc ~/.vimrc
+ln -s `pwd`/.bashrc ~/bashrc
 ln -s `pwd`/.Xmodmap ~/.Xmodmap
 ln -s `pwd`/.ssh/config ~/.ssh/config
