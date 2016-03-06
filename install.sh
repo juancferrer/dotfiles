9fbb3c55245933df25afe99aa5859189c9cce49f
Juan Carlos Ferrer <juan.carlos@micronixsolutions.com>
2016-03-05 18:56:54 -0600

    More updates

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 #TODO: Learn sh foo, and make it a function....
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -s `pwd`/.tmux.conf ~/.tmux.conf
 ln -s `pwd`/.vimrc ~/.vimrc
 mkdir -p ~/.vim/bundle
